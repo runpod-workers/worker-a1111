4f182e2d11db0df19b8e391c3d04e5495419de2e
Justin Merrell <merrelljustin@gmail.com>
2023-05-23 12:07:56 -0400

    Update rp_handler.py

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -3,7 +3,7 @@ import requests
 from requests.adapters import HTTPAdapter, Retry
 
 automatic_session = requests.Session()
-retries = Retry(total=6, backoff_factor=10, status_forcelist=[502, 503, 504])
+retries = Retry(total=30, backoff_factor=0.1, status_forcelist=[502, 503, 504])
 automatic_session.mount('http://', HTTPAdapter(max_retries=retries))
 
 
