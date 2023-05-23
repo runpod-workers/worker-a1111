69d65da343f66223820ee9a2b0ee4a4001100d56
Justin Merrell <merrelljustin@gmail.com>
2023-05-23 16:18:07 -0400

    testing sdp

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -4,6 +4,7 @@ import runpod
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
+
 automatic_session = requests.Session()
 retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
 automatic_session.mount('http://', HTTPAdapter(max_retries=retries))
--- src/start.sh
+++ src/start.sh
@@ -3,7 +3,8 @@
 echo "Worker Initiated"
 
 echo "Starting WebUI API"
-python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --no-tests --skip-install --ckpt /model.safetensors --lowram --xformers --force-enable-xformers --xformers-flash-attention --listen --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+# python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --no-tests --skip-install --ckpt /model.safetensors --lowram --xformers --force-enable-xformers --xformers-flash-attention --listen --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --no-tests --skip-install --ckpt /model.safetensors --lowram --opt-sdp-attention --listen --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
 
 echo "Starting RunPod Handler"
 python -u /rp_handler.py
