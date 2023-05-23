66ca44f3c76358d1b72d2d0d56b40faee62dfab6
Justin Merrell <merrelljustin@gmail.com>
2023-05-23 07:57:18 -0400

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Start the init system
-python -m /stable-diffusion-webui/webui --skip-python-version-check --skip-torch-cuda-test --no-tests --skip-install --ckpt /model.safetensors --lowram --xformers --force-enable-xformers --xformers-flash-attention --listen --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+python -m /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --no-tests --skip-install --ckpt /model.safetensors --lowram --xformers --force-enable-xformers --xformers-flash-attention --listen --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
 
 python -u /rp_handler.py
