ffa6f3a351ce19d4535dff7ed138cd1e3561d12e
Justin Merrell <merrelljustin@gmail.com>
2023-05-23 09:29:58 -0400

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -3,7 +3,7 @@
 echo "Worker Initiated"
 
 echo "Starting WebUI API"
-cd /stable-diffusion-webui && python -m webui.py --skip-python-version-check --skip-torch-cuda-test --no-tests --skip-install --ckpt /model.safetensors --lowram --xformers --force-enable-xformers --xformers-flash-attention --listen --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --no-tests --skip-install --ckpt /model.safetensors --lowram --xformers --force-enable-xformers --xformers-flash-attention --listen --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
 
 echo "Starting RunPod Handler"
 python -u /rp_handler.py
