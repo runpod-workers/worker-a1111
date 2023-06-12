33800964580653d7f8dc220cc4c1571092348cfb
metter <markus.etter@gmail.com>
2023-06-12 12:25:59 +0200

    this should work

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -1,14 +1,11 @@
 import time
-
 import runpod
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
-
 automatic_session = requests.Session()
 retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
 automatic_session.mount('http://', HTTPAdapter(max_retries=retries))
-
 
 # ---------------------------------------------------------------------------- #
 #                              Automatic Functions                             #
@@ -28,15 +25,21 @@ def wait_for_service(url):
 
         time.sleep(0.2)
 
-
-def run_inference(inference_request):
+def txt2img_inference(inference_request):
     '''
-    Run inference on a request.
+    Run inference using the txt2img API.
     '''
     response = automatic_session.post(url='http://127.0.0.1:3000/sdapi/v1/txt2img',
                                       json=inference_request, timeout=600)
     return response.json()
 
+def img2img_inference(inference_request):
+    '''
+    Run inference using the img2img API.
+    '''
+    response = automatic_session.post(url='http://127.0.0.1:3000/sdapi/v1/img2img',
+                                      json=inference_request, timeout=600)
+    return response.json()
 
 # ---------------------------------------------------------------------------- #
 #                                RunPod Handler                                #
@@ -46,10 +49,85 @@ def handler(event):
     This is the handler function that will be called by the serverless.
     '''
 
-    json = run_inference(event["input"])
+    try:
+        input_data = event["input"]
 
-    # return the output that you want to be returned like pre-signed URLs to output artifacts
-    return json
+        # Check if 2step is true in the JSON payload
+        if input_data.get("2step", True):
+
+            # Get the assembly instructions from the "pos" field
+            assembly_instructions = "[frontpad][camera] shot of [prompt][backpad]"
+
+            # Replace the placeholders in the txt2img assembly instructions with corresponding values
+            txt2img_assembled_prompt = txt2img_assembly_instructions.replace(
+                "[frontpad]", "greyscale, cinematic,"
+            ).replace(
+                "[backpad]", ", professional lighting"
+            ).replace(
+                "[camera]", input_data.get("camera", "")
+            ).replace(
+                "[prompt]", input_data.get("prompt", "")
+            ).replace(
+                "[lora]", input_data.get("lora", "")
+            )
+
+            # Update the input data with the assembled prompt
+            input_data["prompt"] = assembled_prompt
+
+
+            # Make a txt2img request
+            txt2img_response = txt2img_inference(input_data)
+
+            # Extract the generated image from the txt2img response
+            generated_image = txt2img_response["images"][0]
+
+            # Update the input data for the img2img request
+            input_data["init_images"] = [generated_image]
+
+            # Get the assembly instructions from the "pos" field
+            assembly_instructions = input_data.get("pos", "")
+
+            # Replace the placeholders in the assembly instructions with corresponding values
+            assembled_prompt = assembly_instructions.replace(
+                "[frontpad]", input_data.get("frontpad", "")
+            ).replace(
+                "[backpad]", input_data.get("backpad", "")
+            ).replace(
+                "[camera]", input_data.get("camera", "")
+            ).replace(
+                "[prompt]", input_data.get("prompt", "")
+            ).replace(
+                "[lora]", input_data.get("lora", "")
+            )
+
+            # Update the input data with the assembled prompt
+            input_data["prompt"] = assembled_prompt
+
+            # Make an img2img request using the updated input data
+            img2img_response = img2img_inference(input_data)
+
+            # Return the final generated image
+            return img2img_response.json()
+        else:
+            # Make a regular txt2img request
+            json_response = txt2img_inference(input_data)
+
+            # Return the regular response
+            return json_response
+    except Exception as e:
+        # Return a JSON error response
+        error_message = "An error occurred: " + str(e)
+        error_response = {
+            "detail": [
+                {
+                    "loc": ["handler"],
+                    "msg": error_message,
+                    "type": "handler_error"
+                }
+            ]
+        }
+        return error_response
+
 
 
 if __name__ == "__main__":
