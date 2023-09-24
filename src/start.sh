adba9193f1882d72a3449c5831009866a46115c7
Justin Merrell <merrelljustin@gmail.com>
2023-09-24 11:38:57 -0400

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -3,7 +3,7 @@
 echo "Worker Initiated"
 
 echo "Starting WebUI API"
-python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --no-tests --skip-install --ckpt /model.safetensors --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /model.safetensors --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
 
 echo "Starting RunPod Handler"
 python -u /rp_handler.py
