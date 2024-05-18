28749140830d04a9b545224ec3220c3799f28ba8
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-05-18 20:35:06 +0200

    Properly implement tcmalloc, prevent spamming logs, and implement xformers

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -1,5 +1,4 @@
 import time
-
 import runpod
 import requests
 from requests.adapters import HTTPAdapter, Retry
@@ -15,15 +14,21 @@ automatic_session.mount('http://', HTTPAdapter(max_retries=retries))
 #                              Automatic Functions                             #
 # ---------------------------------------------------------------------------- #
 def wait_for_service(url):
-    '''
+    """
     Check if the service is ready to receive requests.
-    '''
+    """
+    retries = 0
+
     while True:
         try:
             requests.get(url, timeout=120)
             return
         except requests.exceptions.RequestException:
-            print("Service not ready yet. Retrying...")
+            retries += 1
+
+            # Only log every 15 retries so the logs don't get spammed
+            if retries % 15 == 0:
+                print("Service not ready yet. Retrying...")
         except Exception as err:
             print("Error: ", err)
 
@@ -31,9 +36,9 @@ def wait_for_service(url):
 
 
 def run_inference(inference_request):
-    '''
+    """
     Run inference on a request.
-    '''
+    """
     response = automatic_session.post(url=f'{LOCAL_URL}/txt2img',
                                       json=inference_request, timeout=600)
     return response.json()
@@ -43,9 +48,9 @@ def run_inference(inference_request):
 #                                RunPod Handler                                #
 # ---------------------------------------------------------------------------- #
 def handler(event):
-    '''
+    """
     This is the handler function that will be called by the serverless.
-    '''
+    """
 
     json = run_inference(event["input"])
 
@@ -54,8 +59,6 @@ def handler(event):
 
 
 if __name__ == "__main__":
-    wait_for_service(url=f'{LOCAL_URL}/txt2img')
-
-    print("WebUI API Service is ready. Starting RunPod...")
-
+    wait_for_service(url=f'{LOCAL_URL}/sd-models')
+    print("WebUI API Service is ready. Starting RunPod Serverless...")
     runpod.serverless.start({"handler": handler})
--- src/start.sh
+++ src/start.sh
@@ -1,9 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Worker Initiated"
 
 echo "Starting WebUI API"
-python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /model.safetensors --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
+export LD_PRELOAD="${TCMALLOC}"
+export PYTHONUNBUFFERED=true
+python /stable-diffusion-webui/webui.py \
+  --xformers \
+  --no-half-vae \
+  --skip-python-version-check \
+  --skip-torch-cuda-test \
+  --skip-install \
+  --ckpt /model.safetensors \
+  --lowram \
+  --opt-sdp-attention \
+  --disable-safe-unpickle \
+  --port 3000 \
+  --api \
+  --nowebui \
+  --skip-version-check \
+  --no-hashing \
+  --no-download-sd-model > /webui.log 2>&1 &
 
 echo "Starting RunPod Handler"
 python -u /rp_handler.py
